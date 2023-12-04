0fb2f256acc5a0a5cb2d66850908e06402545d7e
iksoocho <jogaeya@gmail.com>
2023-12-05 08:19:35 +0900

    12/5 오전8시19분

--- vue/user-project/src/layouts/Header.vue
+++ vue/user-project/src/layouts/Header.vue
@@ -1,28 +1,32 @@
 <template>
-    <div>
-        <ul class="nav">
-            <li class="nav-item">
-                
-                <router-link class="nav-link active" to="/">전체 조회</router-link>
-            </li>
-            <li class="nav-item">
-                
-                <router-link class="nav-link" to="/userInsert">등록</router-link>
-            </li>
-           
-            <li class="nav-item">
-                <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-            </li>
-        </ul>
-    </div>
+	<nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+		<div class="container-fluid">
+			<router-link class="navbar-brand" aria-current="page" to="/">User-project</router-link>
+			<button
+				class="navbar-toggler"
+				type="button"
+				data-bs-toggle="collapse"
+				data-bs-target="#navbarNavDropdown"
+				aria-controls="navbarNavDropdown"
+				aria-expanded="false"
+				aria-label="Toggle navigation"
+			>
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNavDropdown">
+				<ul class="navbar-nav">
+					<li class="nav-item">
+						<router-link class="nav-link active" aria-current="page" to="/">전체 조회</router-link>
+					</li>
+					<li class="nav-item">
+						<router-link class="nav-link active" aria-current="page" to="/userInsert">등록</router-link>
+					</li>
+				</ul>
+				<!-- <form class="d-flex" role="search">
+					<input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+					<button class="btn btn-outline-success" type="submit">Search</button>
+				</form> -->
+			</div>
+		</div>
+	</nav>
 </template>
-
-<script>
-export default {
-
-}
-</script>
-
-<style>
-
-</style>
--- vue/user-project/vue.config.js
+++ vue/user-project/vue.config.js
@@ -9,7 +9,7 @@ module.exports = defineConfig({
       '^/api':{                               //^/api로 한 이유는 다른 서버도 받아오기 위해서(api는 그냥 변수 아무거나 준거)
         target,
         changeOrigin : true,
-        ws : false,
+        ws : false,     //웹소켓
         pathRewrite : { '^/api' : '/'}     //경로 재설정  /api 경로를 / 로 재설정해라
       }
     }
