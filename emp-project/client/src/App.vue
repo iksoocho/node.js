4b550b29a9763fa2b993362cb7f60debc6b50122
iksoocho <jogaeya@gmail.com>
2023-12-06 17:51:16 +0900

    12/06 오후 5시 51분

--- emp-project/client/src/App.vue
+++ emp-project/client/src/App.vue
@@ -1,11 +1,18 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+ <div>
+  <Header />
   <router-view/>
+</div>
 </template>
+<script>
+  import Header from './layouts/Header.vue'
 
+  export default{
+  components : {
+    Header
+  }
+}
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
